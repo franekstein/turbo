2cc872b014e634f6e1cbfe547dfdd2d6e754decf
Damian Kieliszek <damian.kieliszek@gmail.com>
2026-02-21 22:21:40 +0100

    feat: add monorepo

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,28 @@
+import { Instrument_Sans, Geist_Mono } from "next/font/google";
+
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+
+const fontSans = Instrument_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
